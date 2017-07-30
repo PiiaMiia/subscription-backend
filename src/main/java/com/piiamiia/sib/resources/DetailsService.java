57f9c0ec563597adea8ba7c40b353d4a42c6c2d8
piiamiia <orav.piia@gmail.com>
2017-07-31 00:56:16 +0300

    added restoreFunction POST method

--- src/main/java/com/piiamiia/sib/resources/DetailsService.java
+++ src/main/java/com/piiamiia/sib/resources/DetailsService.java
@@ -28,4 +28,13 @@ public class DetailsService {
         DetailsDB.setDetails(empty);
         return getDetails();
     }
+
+    @POST
+    @Timed
+    @Produces(MediaType.APPLICATION_JSON)
+    public Details restoreSubscription() {
+        Details empty = new Details("May 13, 2017", "Premium account for €2.99/month","GO Service");
+        DetailsDB.setDetails(empty);
+        return getDetails();
+    }
 }
